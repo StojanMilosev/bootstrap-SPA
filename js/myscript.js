bb89ba4e1f5b43614106bd14948117e480528990
stojan.milosev <stojan.milosev@yahoo.com>
2017-02-25 16:40:00 +0100

    carousel styling

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,7 @@ $(function() {
   "use strict";
 
   var topoffset = 50; //variable for menu height
+  var slideqty = $('#featured .item').length;
 
   //Activate Scrollspy
   $('body').scrollspy({
@@ -10,6 +11,7 @@ $(function() {
     offset: topoffset
   });
 
+  // add inbody class
   var hash = $(this).find('li.active a').attr('href');
   if(hash !== '#featured') {
     $('header nav').addClass('inbody');
@@ -27,6 +29,30 @@ $(function() {
       $('header nav').removeClass('inbody');
     }
   });
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  //Automatically generate carousel indicators
+  for (var i=0; i < slideqty; i++) {
+    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+    $('#featured ol').append(insertText);
+  }
+
 
 
   $('.carousel').carousel({
